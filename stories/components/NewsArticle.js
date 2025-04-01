186825722c207bd1bbad283a2f97618e90d91ae2
Erik Runyon <erunyon@gmail.com>
2025-04-01 10:07:14 -0400

    Add "News Article" component

---
+++ stories/components/NewsArticle.js
@@ -0,0 +1,60 @@
+// filepath: /Users/erunyon/Sites/ndt4-storybook/stories/components/NewsArticle.js
+/**
+ * Creates a news article component
+ * @param {Object} props - Component properties
+ * @param {string} props.title - The title of the news article
+ * @param {boolean} props.showImage - Whether to show the article image
+ * @param {string} props.layout - The layout of the article (vertical or horizontal)
+ * @param {string} props.label - The label of the article
+ * @param {string} props.link - The link to the full article
+ * @param {string} props.author - The author of the article
+ * @param {string} props.publishDate - The publish date of the article
+ * @param {string} props.headingTag - The heading tag to use (h1, h2, h3, etc.)
+ * @returns {HTMLElement} - The news article element
+ */
+export default function NewsArticle(props) {
+  const {
+    title,
+    showImage = true,
+    layout = 'vertical',
+    label = 'Card Label',
+    link = '#',
+    author = 'Author Name',
+    publishDate = 'April 1, 2025',
+    headingTag = 'h2'
+  } = props;
+
+  const container = document.createElement('article');
+  const layoutClasses = layout === 'horizontal' ? 'card--horizontal' : '';
+  container.className = `article snippet card ${layoutClasses}`;
+  container.setAttribute('typeof', 'NewsArticle');
+
+  let articleHTML = '';
+
+  if (showImage) {
+    articleHTML += `
+      <figure class="card-image entry-image">
+        <img property="image" width="600" height="400" loading="lazy"
+             src="/images/placeholder-campus-1-600x400.jpg"
+             alt="Placeholder image">
+      </figure>
+    `;
+  }
+
+  articleHTML += `
+    <div class="card-body">
+      <p class="card-label">${label}</p>
+      <${headingTag} class="article-title card-title" property="headline">
+        <a class="card-link" href="${link}">${title}</a>
+      </${headingTag}>
+      <div class="article-meta">
+        <link property="publisher" resource="#siteorg">
+        <div property="author" typeof="Person"><meta property="name" content="${author}"></div>
+        <p class="meta-item publish-info"><time property="datePublished" datetime="${new Date(publishDate).toISOString()}">${publishDate}</time></p>
+      </div>
+    </div>
+  `;
+
+  container.innerHTML = articleHTML;
+  return container;
+}
---
+++ stories/components/NewsArticle.stories.js
@@ -0,0 +1,73 @@
+// filepath: /Users/erunyon/Sites/ndt4-storybook/stories/components/NewsArticle.stories.js
+import NewsArticle from './NewsArticle.js';
+
+export default {
+  title: 'Components/NewsArticle',
+  tags: ['autodocs'],
+  argTypes: {
+    title: { name: 'Article Title' },
+    showImage: {
+      name: 'Show Image',
+      control: 'boolean',
+      description: 'Toggle to show or hide the article image'
+    },
+    layout: {
+      name: 'Layout',
+      control: 'select',
+      options: ['vertical', 'horizontal'],
+      description: 'Choose between vertical and horizontal layout'
+    },
+    label: { name: 'Card Label' },
+    link: { name: 'Article Link' },
+    author: { name: 'Author Name' },
+    publishDate: { name: 'Publish Date' },
+    headingTag: {
+      name: 'Heading Tag',
+      control: 'select',
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    }
+  },
+  args: {
+    title: 'Notre Dame Research Discovers New Method to Address Climate Change',
+    showImage: true,
+    layout: 'vertical',
+    label: 'Research',
+    link: '#',
+    author: 'Jane Smith',
+    publishDate: 'April 1, 2025',
+    headingTag: 'h2'
+  }
+};
+
+export const Default = (args) => {
+  return NewsArticle(args);
+};
+
+export const HorizontalLayout = (args) => {
+  return NewsArticle(args);
+};
+HorizontalLayout.args = {
+  layout: 'horizontal'
+};
+
+export const WithoutImage = (args) => {
+  return NewsArticle(args);
+};
+WithoutImage.args = {
+  showImage: false
+};
+
+export const HorizontalWithoutImage = (args) => {
+  return NewsArticle(args);
+};
+HorizontalWithoutImage.args = {
+  layout: 'horizontal',
+  showImage: false
+};
+
+export const WithDifferentHeading = (args) => {
+  return NewsArticle(args);
+};
+WithDifferentHeading.args = {
+  headingTag: 'h3'
+};
