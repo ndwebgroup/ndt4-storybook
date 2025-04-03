030a40d5e0a0b7c8cb31a189afa29619a4d10767
Erik Runyon <erunyon@gmail.com>
2025-04-03 13:13:15 -0400

    Add `span.icon` conversion javascript

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -64,6 +64,33 @@ const preview = {
   decorators: [withSvgSprite],
 };
 
+/*!
+ * Load ND icons
+ * Replaces a `span` with the `.icon` class
+ * <span class="icon" data-icon="calendar"></span>
+ */
+const loadIcons = () => {
+  let icons = document.querySelectorAll('span.icon');
+  for(let i=0; i< icons.length; i++){
+    let el = icons[i],
+        className = el.getAttribute('class'),
+        dataIcon = el.getAttribute('data-icon'),
+        svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+        use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+    ;
+    use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#icon-' + dataIcon);
+    svg.setAttribute('class', className);
+    svg.setAttribute('data-icon', dataIcon);
+    svg.setAttribute('aria-hidden', true);
+    svg.setAttribute('focusable', false);
+    svg.appendChild(use);
+    el.parentNode.replaceChild(svg, el);
+  }
+};
+document.addEventListener('DOMContentLoaded', function() {
+  // loadIcons();
+});
+
 const body = document.body;
 body.classList.add("sb-unstyled");
 
