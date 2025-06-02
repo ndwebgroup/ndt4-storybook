1c07780057c8e9073d642496acd30fb6b9bf1015
Aaron Greene <agreene5@nd.edu>
2025-06-02 16:33:54 -0400

    Update site header markup

--- stories/collections/Header.js
+++ stories/collections/Header.js
@@ -27,44 +27,28 @@ export default function Header(props) {
   // Create the header HTML
   const headerHTML = `
     <header id="header" class="site-header">
-    <p class="mark-header"><a href="https://www.nd.edu/">University of Notre Dame</a></p>
-
-    <div class="site-title-group has-parent">
-      <h1 id="site-title" class="site-title "><a href="/" accesskey="1" title="Homepage shortcut key = 1">${siteName}</a></h1>
+    <a class="header-mark-mobile" href="https://www.nd.edu/" title="University of Notre Dame">
+      <svg width="512" height="86" aria-hidden="true" alt="University of Notre Dame"><use xlink:href="#mobile-mark"></use></svg>
+      <span class="visually-hidden">University of Notre Dame</span>
+    </a>
+    <div class="header-group">
+      <div class="header-title">
+        <svg class="header-mark" width="250" height="60" aria-hidden="true" alt="University of Notre Dame"><use xlink:href="#academic-mark"></use></svg>
+        <div class="header-title-name">
+          <h1 id="site-title" class="site-title "><a href="/" accesskey="1" title="Homepage shortcut key = 1">${siteName}</a></h1>
+        </div>
+      </div>
+      ${showNavigation ? `<!-- Header Search/Nav  -->
+      <div class="header-nav-wrapper">
+        <div class="header-nav">${navElement}</div>
+      </div>` : ''}
+      <div class="header-util">
+        <div class="header-nav-toggle">
+    	    <button class="btn--action global-menu-toggle" aria-label="Open global menu and search" aria-controls="global-menu" aria-haspopup="dialog"><svg alt="Global Menu"><use xmlns:xlink="https://www.w3.org/1999/xlink" xlink:href="#icon-search-menu"></use></svg></button>
+    		</div>
+      </div>
     </div>
-
-    ${showNavigation ? `
-    <!-- Header Search/Nav  -->
-    <div class="nav-header">
-      ${navElement}
-    </div>
-    ` : ''}
-
-    <!-- Mobile Navbar -->
-    <div class="nav-mobile-util">
-      <ul class="no-bullets">
-        <li><a href="/"><svg class="icon" data-icon="home" width="16" height="16"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-home"></use></svg> Home</a></li>
-        <li><button type="button" class="btn-search search-toggle"><svg class="icon" data-icon="search" width="16" height="16"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-search"></use></svg> Search</button></li>
-        <li>
-          <a href="#nav" class="nav-menu nav-skip">
-          <svg version="1.1" class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 20 20" xml:space="preserve">
-            <line class="ni ni1" stroke-width="2" stroke-linecap="square" stroke-miterlimit="10" x1="0.98" y1="2" x2="18.94" y2="2"></line>
-            <line class="ni ni2" stroke-width="2" stroke-linecap="square" stroke-miterlimit="10" x1="0.98" y1="8.69" x2="18.94" y2="8.69"></line>
-          </svg>
-          <span class="ni ni3">Menu</span>
-        </a>
-        </li>
-      </ul>
-      <search title="Site search" role="search" class="nav-search-wrapper">
-        <form method="get" action="/search/" id="search-navbar-mobile" class="search-form" role="search" aria-label="Site search">
-          <input type="hidden" name="as_sitesearch" value="ndt4.conductor.nd.edu">
-          <input type="hidden" name="entqr" value="3">
-          <input type="search" name="q" class="search-input" id="search-input-navbar-mobile" placeholder="Search this site" title="type your search term" aria-label="Site Search input">
-          <button type="submit" class="search-button" aria-label="Search"><svg class="icon" data-icon="search" width="16" height="16"><use xlink:href="#icon-search"></use></svg></button>
-          <button type="button" class="search-close search-toggle" aria-label="Close Search"><svg class="icon" data-icon="close" width="16" height="16"><use xlink:href="#icon-close"></use></svg></button>
-        </form>
-      </search>
-    </div>
+    
   </header>
   `;
 
--- stories/components/NavigationPrimary.js
+++ stories/components/NavigationPrimary.js
@@ -20,13 +20,12 @@ export default function NavigationPrimary(args) {
   const { items = defaultNavigationItems } = args;
   const navItems = Array.isArray(items) ? items : defaultItems;
   return `
-<nav id="nav-top" class="nav-top" aria-label="Primary">
+<nav id="nav-primary" class="nav-primary" aria-label="Primary">
   <div id="primary">
     <ul>
       ${navItems.map((item) => {
           return `<li><a href="${item.url ? item.url : '#' }">${item.label}</a></li>`;
         }).join('\n      ')}
-      <li class="nav-search"><button type="button" class="btn-search search-toggle" aria-label="Toggle search"><svg class="icon" data-icon="search" width="16" height="16"><use xlink:href="#icon-search"></use></svg></button></li>
     </ul>
   </div>
 </nav>
