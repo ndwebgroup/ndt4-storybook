dedd522155416835e0c24150921b2efca416527d
Aaron Greene <agreene5@nd.edu>
2025-09-26 21:54:34 -0400

    Minor update to Notice markup

--- stories/components/Notice.js
+++ stories/components/Notice.js
@@ -11,7 +11,7 @@ export default function Notice(args) {
 
   const variantClass = variant !== 'default' ? `notice--${variant}` : '';
   const iconMarkup = icon ? `<span class="icon" data-icon="${icon}"></span>` : '';
-  const exampleLink = hasLink ? `<a class="notice-link" href="#">an example link</a>` : '';
+  const exampleLink = hasLink ? `<a href="#"><strong>an example link</strong></a>` : '';
   const contentWithLink = hasLink ? content.replace('{link}', exampleLink) : content;
 
   return `
