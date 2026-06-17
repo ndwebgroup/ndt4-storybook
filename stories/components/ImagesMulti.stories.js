7ccde722708eda5f5cd2b2f13aeca1514145b9bf
aaron0574 <aarongreenedesign@gmail.com>
2026-06-17 14:51:37 -0400

    Update arg formatting for Multi Image component

--- stories/components/ImagesMulti.stories.js
+++ stories/components/ImagesMulti.stories.js
@@ -4,12 +4,6 @@ export default {
   title: 'Components/Image (Multiple)',
   tags: ['stable'],
   parameters: {
-    design: {
-      name: "Figma Assets",
-      allowFullscreen: true,
-      type: "figma",
-      url: "https://www.figma.com/design/oMHFAa6HK6xMGMtxPhOkLV/University-of-Notre-Dame---Web-Theme-4.0-Digital-Design-Guide?node-id=3352-24148",
-    },
     docs: {
       description: {
         component: 'The multi-image component allows for displaying multiple images in a mosaic or tiled layout. The mosaic variant creates a dynamic, organically arranged layout of images, while the tiled variant arranges images in a clean grid. Both variants support up to 4 images and can be used to create visually engaging galleries or collections of related images.'
@@ -32,15 +26,29 @@ export default {
 export const Default = (args) => {
   return MultiImage(args);
 };
+Default.args = {
+};
 
 export const Mosaic = (args) => {
-  return MultiImage({ ...args, imageVariant: 'mosaic', imageCount: args.imageCount });
+  return MultiImage(args);
+};
+Mosaic.args = {
+  imageVariant: 'mosaic',
+  imageCount:3
 };
 
 export const Tiled = (args) => {
-  return MultiImage({ ...args, imageVariant: 'tiled', imageCount: args.imageCount });
+  return MultiImage(args);
 };
+Tiled.args = {
+  imageVariant: 'tiled',
+  imageCount: 3
+}; 
 
 export const WithImageNumbers = (args) => {
-  return MultiImage({ ...args, imageVariant: 'tiled', imageCount: args.imageCount, imageNumber: true });
+  return MultiImage(args);
 }
+WithImageNumbers.args = {
+  imageCount: 3,
+  imageNumber: true,
+};
