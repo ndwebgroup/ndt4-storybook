15e02a9eefea69ac28f9ddf4de62f9bab607cca1
aaron0574 <aarongreenedesign@gmail.com>
2026-06-19 09:49:43 -0400

    Update Gallery args to include numbers

--- stories/components/Gallery.js
+++ stories/components/Gallery.js
@@ -4,16 +4,17 @@
  * @param {string} props.id - The ID for the gallery
  * @param {string} [props.classes] - Additional CSS classes to apply to the gallery container
  * @param {number} props.images - Number of images in the gallery
+ * @param {boolean} [props.showNumber] - Whether to show the image number as overlay on the images
  * @returns {string} - The gallery HTML
  */
 export default function Gallery(props) {
-  const { id, classes, images } = props;
+  const { id, classes, images, showNumber } = props;
 
   return `
     <div class="gallery-wrapper${classes ? ' ' + classes : ''}">
       <ul id="gallery-${id}" class="gallery-lb gallery-${id}" data-count="${images}">
         ${Array.from({ length: images }, (_, i) => `
-          <li>
+          <li ${showNumber ? ' class="show-number"' : ''}">
             <a href="#" title="" data-title="Image ${i + 1}">
               <img
                 src="/images/placeholder-campus-1-600x600.jpg"
--- stories/components/Gallery.stories.js
+++ stories/components/Gallery.stories.js
@@ -1,5 +1,37 @@
 import Gallery from './Gallery.js';
 
+// Add styles to document head
+const addHeaderStyles = () => {
+  if (!document.getElementById('header-styles')) {
+    const style = document.createElement('style');
+    style.id = 'header-styles';
+    style.textContent = `
+      .show-number {
+        position:relative;
+        counter-increment:list-counter;
+      }
+      .show-number::after {
+        content:counter(list-counter);
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        text-align:center;
+        position:absolute;
+        inset:0;
+        background:rgba(12,35,64,0.75);
+        color:#fff;
+        font-size:2rem;
+        font-weight:bold;
+        border-radius:4px;
+      }
+      .gallery--tiled .show-number::after {
+        inset:0.5rem;
+      }
+    `;
+    document.head.appendChild(style);
+  }
+};
+
 // Gallery CSS and JavaScript resources
 const galleryResources = `
 <link rel="stylesheet" href="https://conductor.nd.edu/stylesheets/lb.css">
@@ -28,7 +60,15 @@ export default {
     },
     layout: 'padded'
   },
-  decorators: [withGalleryResources],
+  decorators: [
+    withGalleryResources,
+    (Story) => {
+      // Add styles to document head
+      addHeaderStyles();
+      // This will only affect the preview
+      return Story();
+    }
+  ],
   argTypes: {
     id: {
       name: 'Gallery ID',
@@ -43,67 +83,77 @@ export default {
     images: {
       name: 'Gallery Images',
       description: 'Number of images to display in the gallery',
-      control: { type: 'range', min:1, max:40, step: 1 },
+      control: { type: 'number', min:1, max:40, step: 1 },
+    },
+    showNumber: {
+      name: 'Show Image Number',
+      description: 'Whether to show the image number as overlay on the images',
+      control: 'boolean'
     }
   },
   args: {
+    // Default args for all stories
     id: '15',
     classes: '',
     images: 15,
+    showNumber: false,
     imageSrc: '/images/placeholder-campus-3-1200x675.jpg',
   }
 };
 
 export const Default = (args) => {
   return Gallery(args);
-  
+};
+Default.args = {
 };
 
 export const TwoImages = (args) => {
-  return Gallery({
-    ...args,
-    images: 2
-  });
+  return Gallery(args);
+};
+TwoImages.args = {
+  id: "2",
+  images:2
 };
 
 export const ThreeImages = (args) => {
-  return Gallery({
-    ...args,
-    id: "3",
-    images: 3
-  });
+  return Gallery(args);
 };
+ThreeImages.args = {
+  id: "3",
+  images:3
+}; 
 
 export const FiveImages = (args) => {
-  return Gallery({
-    ...args,
-    id: "5",
-    images: 5
-  });
+  return Gallery(args);
 };
+FiveImages.args = {
+  id: "5",
+  images:5
+}; 
 
 export const TwelveImages = (args) => {
-  return Gallery({
-    ...args,
-    id: "12",
-    images: 12
-  });
+  return Gallery(args);
+};
+TwelveImages.args = {
+  id: "12",
+  images:12
 };
 
+
 export const GalleryTiled = (args) => {
-  return Gallery({
-    ...args,
-    id: "15",
-    classes: 'gallery--tiled',
-    images: args.images
-  });
+  return Gallery(args);
+};
+GalleryTiled.args = {
+  id: "15",
+  classes: 'gallery--tiled',
+  images: 15
 };
 
 export const GallerySlider = (args) => {
-  return Gallery({
-    ...args,
-    id: "21",
-    classes: 'gallery--slider',
-    images: args.images
-  });
+  return Gallery(args);
+};
+GallerySlider.args = {
+  id: "21",
+  classes: 'gallery--slider',
+  images: 21 
 };
